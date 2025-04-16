e753de0e9af177ef667ec3057f33163babb98abe
Pawel <pencak.pawel@gmail.com>
2025-04-16 16:55:44 +0100

    Basic setup

---
+++ src/test/java/com/example/dataBaseUpdater/DataBaseUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataBaseUpdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseUpdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
